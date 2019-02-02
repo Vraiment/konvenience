dba78b656fc7ff04bd5f8a3bcb66819b4265004a
Vraiment <jemc44@gmail.com>
2019-02-02 00:44:56 -0800

    Add "Also()" unit tests

---
+++ KonvenienceTests/CoreTests.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using FluentAssertions;
+using NUnit.Framework;
+
+namespace Konvenience.Tests
+{
+    class CoreTests
+    {
+        [Test]
+        public void Test_Using_Also_In_An_Object()
+        {
+            var dummy = new DummyClass();
+
+            var result = dummy.Also(d => d.Configure());
+
+            result.Should().BeSameAs(dummy);
+        }
+
+        [Test]
+        public void Test_Using_Also_With_A_Null_Object()
+        {
+            string value = null;
+            var executed = false;
+
+            var result = value.Also(v => executed = true);
+
+            result.Should().BeNull();
+            executed.Should().BeTrue();
+        }
+
+        [Test]
+        public void Test_Using_Also_With_A_Null_Argument()
+        {
+            var dummy = new DummyClass();
+
+            dummy.Invoking(d => d.Also(null))
+                .Should()
+                .Throw<ArgumentNullException>();
+        }
+    }
+}
---
+++ KonvenienceTests/DummyClass.cs
@@ -0,0 +1,9 @@
+﻿namespace Konvenience.Tests
+{
+    public class DummyClass
+    {
+        public bool Configured { get; private set; } = false;
+
+        public void Configure() => Configured = true;
+    }
+}
